1d3581d8f0dbf95a74ecaa74a2bee23be6fb7410
pedroalano <pedroalano@gmail.com>
2025-07-22 12:40:26 -0300

    Set initial java Spring structure

---
+++ src/main/java/br/com/pedroalano/my_pocket/MyPocketApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedroalano.my_pocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedroalano/my_pocket/MyPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedroalano.my_pocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
